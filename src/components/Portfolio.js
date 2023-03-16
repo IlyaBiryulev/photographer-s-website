5b9f7a5d2661e527cd30fd5a88f0bc623b50c73c
Ilya Biryulev <birulevila@yandex.ru>
2023-03-16 21:52:32 +0300

    feat: создать массив из фотографий для portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,10 @@
 import Header from "./Header";
+import Card1 from "../image/Card1.jpg";
+import {cardImage} from "../utils/cardImage.js"
 
+console.log(Card1)
+
+console.log(cardImage)
 function Portfolio() {
   return (
     <div>
@@ -9,7 +14,12 @@ function Portfolio() {
       <main className="portfolio">
         <p className="portfolio__quote">&quot;Большинство вещей в&nbsp;жизни&nbsp;&mdash; это моменты удовольствия и&nbsp;смущения на&nbsp;всю жизнь. Фотография&nbsp;&mdash; это момент смущения и&nbsp;наслаждения на&nbsp;всю жизнь&quot;.&mdash; Тони Бенн</p>
         <section className="cards">
-          <img src="../../image/Card1"></img>
+          <img src= {cardImage.one} className="cards__img" alt="изображение"></img>
+          <img src= {cardImage.two} className="cards__img" alt="изображение"></img>
+          <img src= {cardImage.three} className="cards__img" alt="изображение"></img>
+          <img src= {cardImage.four} className="cards__img" alt="изображение"></img>
+          <img src= {cardImage.five} className="cards__img" alt="изображение"></img>
+          <img src= {cardImage.six} className="cards__img" alt="изображение"></img>
         </section>
       </main>
     </div>
