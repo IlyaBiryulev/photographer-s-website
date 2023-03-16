241ebc4879298cc5ce7508041a996ccaf3ec6866
Ilya Biryulev <birulevila@yandex.ru>
2023-03-16 22:17:32 +0300

    feat: создать вкладку portfolio. сделать grid сетку

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,12 +14,12 @@ function Portfolio() {
       <main className="portfolio">
         <p className="portfolio__quote">&quot;Большинство вещей в&nbsp;жизни&nbsp;&mdash; это моменты удовольствия и&nbsp;смущения на&nbsp;всю жизнь. Фотография&nbsp;&mdash; это момент смущения и&nbsp;наслаждения на&nbsp;всю жизнь&quot;.&mdash; Тони Бенн</p>
         <section className="cards">
-          <img src= {cardImage.one} className="cards__img" alt="изображение"></img>
-          <img src= {cardImage.two} className="cards__img" alt="изображение"></img>
-          <img src= {cardImage.three} className="cards__img" alt="изображение"></img>
-          <img src= {cardImage.four} className="cards__img" alt="изображение"></img>
-          <img src= {cardImage.five} className="cards__img" alt="изображение"></img>
-          <img src= {cardImage.six} className="cards__img" alt="изображение"></img>
+          <a href="#"><img src= {cardImage.one} className="cards__img" alt="изображение" /></a>
+          <a href="#"><img src= {cardImage.two} className="cards__img" alt="изображение" /></a>
+          <a href="#"><img src= {cardImage.three} className="cards__img" alt="изображение" /></a>
+          <a href="#"><img src= {cardImage.four} className="cards__img" alt="изображение" /></a>
+          <a href="#"><img src= {cardImage.five} className="cards__img" alt="изображение" /></a>
+          <a href="#"><img src= {cardImage.six} className="cards__img" alt="изображение" /></a>
         </section>
       </main>
     </div>
