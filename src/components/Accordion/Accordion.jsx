9ac1255b339e3f681a0f3f8833a6bb8759add7e6
Ilya Biryulev <birulevila@yandex.ru>
2023-09-01 13:27:53 +0300

    feat: доделать аккардион

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,24 +1,17 @@
 import './Accordion.css';
-import {React, useState} from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
-function Accordion({item}) {
-  const [ isActive, setIsActive ] = useState(false);
-
-  const handleClick = () => {
-    setIsActive(!isActive)
-  }
-
+function Accordion({item, opened, toggle}) {
   return (
     <article className='accordion'>
       <div className='accordion__item'>
-        <div className='accordion__card' onClick={handleClick}>
+        <div className='accordion__card' onClick={toggle}>
           <p className='accordion__title'>{item.title}</p>
-          <div className={isActive ? 'accordion__plus accordion__plus_close' : 'accordion__plus'}/>
+          <div className={opened ? 'accordion__plus accordion__plus_close' : 'accordion__plus'}/>
         </div>
         <AnimatePresence>
-        {isActive &&
-          <div className='accordion__content'>
+          {opened &&
+            <div className='accordion__content'>
             <motion.p className='accordion__text'
               initial={{ opacity: 0, y: '50px' }}
               animate={{ opacity: 1, y: 0, transition: {duration: 0.3}}}
@@ -41,7 +34,7 @@ function Accordion({item}) {
               {item.content3}
             </motion.p>
           </div>
-        }
+          }
         </AnimatePresence>
       </div>
     </article>
--- src/components/Filming/Filming.jsx
+++ src/components/Filming/Filming.jsx
@@ -1,19 +1,19 @@
 import './Filming.css';
-import {React} from 'react';
+import {useState} from 'react';
 import Header from '../Header/Header';
 import { filmingData } from '../../utils/filmingContent';
 import Accordion from '../Accordion/Accordion';
 
 function Filming() {
-
+  const [opened, setOpened] = useState(null)
 
   return (
     <div className='filming'>
       <Header />
       <div className="filming__item">
         <h1 className='filming__title'>ИНФОРМАЦИЯ О СЪЕМКАХ</h1>
-        {filmingData.map((item) =>
-          <Accordion item={item}/>
+        {filmingData.map((item, i) =>
+          <Accordion item={item} opened={ i === opened } toggle = {setOpened.bind(null, i === opened ? null : i)}/>
         )}
       </div>
     </div>
